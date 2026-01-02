23af6089c97b5ebb0b7162707c16f3fb20d20d8a
SuhaniSharma <Suhanisharma2916@gmail.com>
2026-01-02 19:52:30 +0530

    first commit

---
+++ .history/server_20260102193822.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+// Serve CSS & JS files
+app.get("/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "style.css"));
+});
+
+app.get("/script.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "script.js"));
+});
+
+// Serve HTML pages
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "register.html"));
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
